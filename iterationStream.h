4fc1a70d76ca1232eb5c22a6cbc062e9ad12351c
FrozenLemonTee <1115306170@qq.com>
2024-11-08 18:41:48 +0800

    Add an abstract class in iterationStream.h

---
+++ iterationStream.h
@@ -0,0 +1,14 @@
+#ifndef ITERATIONSTREAM_H
+#define ITERATIONSTREAM_H
+
+#include "printable.h"
+#include "iterable.h"
+
+namespace original{
+    template<typename TYPE>
+    class iterationStream : public printable, public iterable<TYPE>{
+        // todo
+    };
+}
+
+#endif //ITERATIONSTREAM_H
