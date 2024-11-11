5721ad40137b18ab4fd3aca0f42a16656f4d627f
FrozenlemonTee <1115306170@qq.com>
2024-11-11 12:16:16 +0800

    Add couple.h, fix some bugs

--- docs/html/search/all_1.js
+++ docs/html/search/all_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['chain_0',['chain',['../classoriginal_1_1chain.html',1,'original']]],
   ['comparator_1',['comparator',['../classoriginal_1_1comparator.html',1,'original']]],
-  ['container_2',['container',['../classoriginal_1_1container.html',1,'original']]]
+  ['container_2',['container',['../classoriginal_1_1container.html',1,'original']]],
+  ['couple_3',['couple',['../classoriginal_1_1couple.html',1,'original']]]
 ];
