7ec5e5eb6c1e753b3386401bf01a1b7458785924
FrozenLemonTee <1115306170@qq.com>
2024-11-15 13:32:54 +0800

    Add more filters in filter.h

--- filter.h
+++ filter.h
@@ -17,7 +17,47 @@ namespace original {
         TYPE target;
         bool match(const TYPE& t) const override;
     public:
-        explicit equalFilter(const TYPE& threshold);
+        explicit equalFilter(const TYPE& target);
+    };
+
+    template<typename TYPE>
+    class notEqualFilter final : public filter<TYPE> {
+        TYPE target;
+        bool match(const TYPE& t) const override;
+    public:
+        explicit notEqualFilter(const TYPE& target);
+    };
+
+    template<typename TYPE>
+    class lessFilter final : public filter<TYPE> {
+        TYPE low;
+        bool match(const TYPE& t) const override;
+    public:
+        explicit lessFilter(const TYPE& low);
+    };
+
+    template<typename TYPE>
+    class greaterFilter final : public filter<TYPE> {
+        TYPE high;
+        bool match(const TYPE& t) const override;
+    public:
+        explicit greaterFilter(const TYPE& high);
+    };
+
+    template<typename TYPE>
+    class notLessFilter final : public filter<TYPE> {
+        TYPE high;
+        bool match(const TYPE& t) const override;
+    public:
+        explicit notLessFilter(const TYPE& high);
+    };
+
+    template<typename TYPE>
+    class notGreaterFilter final : public filter<TYPE> {
+        TYPE low;
+        bool match(const TYPE& t) const override;
+    public:
+        explicit notGreaterFilter(const TYPE& low);
     };
 
     template<typename TYPE>
@@ -46,8 +86,53 @@ namespace original {
     }
 
     template<typename TYPE>
-    original::equalFilter<TYPE>::equalFilter(const TYPE& threshold)
-            : target(threshold) {}
+    original::equalFilter<TYPE>::equalFilter(const TYPE& target)
+            : target(target) {}
+
+    template<typename TYPE>
+    bool original::notEqualFilter<TYPE>::match(const TYPE& t) const {
+        return t != target;
+    }
+
+    template<typename TYPE>
+    original::notEqualFilter<TYPE>::notEqualFilter(const TYPE& target)
+            : target(target) {}
+
+    template<typename TYPE>
+    bool original::lessFilter<TYPE>::match(const TYPE& t) const {
+        return t < low;
+    }
+
+    template<typename TYPE>
+    original::lessFilter<TYPE>::lessFilter(const TYPE& low)
+            : low(low) {}
+
+    template<typename TYPE>
+    bool original::greaterFilter<TYPE>::match(const TYPE& t) const {
+        return t > high;
+    }
+
+    template<typename TYPE>
+    original::greaterFilter<TYPE>::greaterFilter(const TYPE& high)
+            : high(high) {}
+
+    template<typename TYPE>
+    bool original::notLessFilter<TYPE>::match(const TYPE& t) const {
+        return t >= high;
+    }
+
+    template<typename TYPE>
+    original::notLessFilter<TYPE>::notLessFilter(const TYPE& high)
+            : high(high) {}
+
+    template<typename TYPE>
+    bool original::notGreaterFilter<TYPE>::match(const TYPE& t) const {
+        return t <= low;
+    }
+
+    template<typename TYPE>
+    original::notGreaterFilter<TYPE>::notGreaterFilter(const TYPE& low)
+            : low(low) {}
 
     template<typename TYPE>
     bool original::rangeFilter<TYPE>::match(const TYPE& t) const {
