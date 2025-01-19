d0778f5f5c8d4815f03f9962d832b8da9e045f79
frozenlemontee <frozenlemontee@gmail.com>
2025-01-20 00:53:17 +0800

    refactor: Modify inheritance structure

--- src/core/container.h
+++ src/core/container.h
@@ -11,10 +11,7 @@ namespace original {
     public:
         [[nodiscard]] virtual uint32_t size() const = 0;
         [[nodiscard]] bool empty() const;
-        virtual bool contains(const TYPE& e) const = 0 ;
-        virtual void add(const TYPE& e) = 0;
-        virtual TYPE remove(const TYPE& e) = 0;
-        virtual void clear() = 0;
+        virtual bool contains(const TYPE& e) const = 0;
         virtual ~container() = default;
     };
 }
--- src/core/serial.h
+++ src/core/serial.h
@@ -2,7 +2,6 @@
 #define SERIAL_H
 
 #include "container.h"
-#include "error.h"
 
 namespace original{
     template <typename TYPE>
@@ -11,9 +10,6 @@ namespace original{
         [[nodiscard]] bool indexOutOfBound(int64_t index) const;
         [[nodiscard]] int64_t parseNegIndex(int64_t index) const;
     public:
-        void add(const TYPE& e) override;
-        TYPE remove(const TYPE& e) override;
-        void clear() override;
         virtual TYPE get(int64_t index) const = 0;
         virtual TYPE getBegin() const;
         virtual TYPE getEnd() const;
@@ -22,12 +18,6 @@ namespace original{
         virtual void set(int64_t index, const TYPE& e) = 0;
         virtual uint32_t indexOf(const TYPE& e) const = 0;
         bool contains(const TYPE& e) const override;
-        virtual void push(int64_t index, const TYPE& e);
-        virtual TYPE pop(int64_t index);
-        virtual void pushBegin(const TYPE& e);
-        virtual TYPE popBegin();
-        virtual void pushEnd(const TYPE& e);
-        virtual TYPE popEnd();
     };
 }
 
@@ -41,26 +31,6 @@ namespace original{
     template<typename TYPE>
     auto original::serial<TYPE>::parseNegIndex(int64_t index) const -> int64_t {
         return index >= 0 ? index : this->size() + index;
-    }
-
-    template <typename TYPE>
-    auto original::serial<TYPE>::add(const TYPE &e) -> void
-    {
-        this->pushEnd(e);
-    }
-
-    template <typename TYPE>
-    auto original::serial<TYPE>::remove(const TYPE& e) -> TYPE
-    {
-        const int64_t index = this->indexOf(e);
-        return this->pop(index);
-    }
-
-    template<typename TYPE>
-    auto original::serial<TYPE>::clear() -> void {
-        while (!this->empty()) {
-            this->popEnd();
-        }
     }
 
     template<typename TYPE>
@@ -83,36 +53,6 @@ namespace original{
     auto original::serial<TYPE>::contains(const TYPE &e) const -> bool
     {
         return this->indexOf(e) != this->size();
-    }
-
-    template<typename TYPE>
-    void original::serial<TYPE>::push(int64_t index, const TYPE &e) {
-        throw unSupportedMethodError();
-    }
-
-    template<typename TYPE>
-    TYPE original::serial<TYPE>::pop(int64_t index) {
-        throw unSupportedMethodError();
-    }
-
-    template<typename TYPE>
-    void original::serial<TYPE>::pushBegin(const TYPE &e) {
-        throw unSupportedMethodError();
-    }
-
-    template<typename TYPE>
-    TYPE original::serial<TYPE>::popBegin() {
-        throw unSupportedMethodError();
-    }
-
-    template<typename TYPE>
-    void original::serial<TYPE>::pushEnd(const TYPE &e) {
-        throw unSupportedMethodError();
-    }
-
-    template<typename TYPE>
-    TYPE original::serial<TYPE>::popEnd() {
-        throw unSupportedMethodError();
     }
 
 #endif //SERIAL_H
