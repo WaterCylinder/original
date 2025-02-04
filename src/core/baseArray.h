38ac7c2b24e9302c11373097a347154a27872329
frozenlemontee <frozenlemontee@gmail.com>
2025-02-04 19:51:20 +0800

    refactor: Prohibit direct instantiation of baseArray

--- src/core/baseArray.h
+++ src/core/baseArray.h
@@ -4,7 +4,11 @@
 
 namespace original{
     template <typename TYPE>
-    class baseArray : public serial<TYPE>{};
+    class baseArray : public serial<TYPE> {
+        protected:
+            baseArray() = default;
+            ~baseArray() override = default;
+    };
 }
 
 #endif //BASEARRAY_H
