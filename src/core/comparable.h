5f30cf3d383c5c242de143ae91cf6d6a11ef7820
frozenlemontee <frozenlemontee@gmail.com>
2025-02-06 22:38:01 +0800

    feat: Add comparable.h

---
+++ src/core/comparable.h
@@ -0,0 +1,49 @@
+#ifndef COMPARABLE_H
+#define COMPARABLE_H
+
+namespace original {
+    template <typename DERIVED>
+    class comparable {
+        public:
+            virtual int compareTo(const DERIVED &other) const = 0;
+            bool operator==(const DERIVED &other) const;
+            bool operator!=(const DERIVED &other) const;
+            bool operator<(const DERIVED &other) const;
+            bool operator>(const DERIVED &other) const;
+            bool operator<=(const DERIVED &other) const;
+            bool operator>=(const DERIVED &other) const;
+            virtual ~comparable() = default;
+    };
+
+    template<typename DERIVED>
+    auto comparable<DERIVED>::operator==(const DERIVED &other) const -> bool {
+        return compareTo(other) == 0;
+    }
+
+    template<typename DERIVED>
+    auto comparable<DERIVED>::operator!=(const DERIVED &other) const -> bool {
+        return compareTo(other) != 0;
+    }
+
+    template<typename DERIVED>
+    auto comparable<DERIVED>::operator<(const DERIVED &other) const -> bool {
+        return compareTo(other) < 0;
+    }
+
+    template<typename DERIVED>
+    auto comparable<DERIVED>::operator>(const DERIVED &other) const -> bool {
+        return compareTo(other) > 0;
+    }
+
+    template<typename DERIVED>
+    auto comparable<DERIVED>::operator<=(const DERIVED &other) const -> bool {
+        return compareTo(other) <= 0;
+    }
+
+    template<typename DERIVED>
+    auto comparable<DERIVED>::operator>=(const DERIVED &other) const -> bool {
+        return compareTo(other) >= 0;
+    }
+}
+
+#endif //COMPARABLE_H
