435d2f502f3ec15e5ab5f9f003fc01680b6a4300
frozenlemontee <frozenlemontee@gmail.com>
2025-02-14 12:01:39 +0800

    docs: Main file docs modified

--- src/original.cpp
+++ src/original.cpp
@@ -1,40 +1,48 @@
 /**
- * 项目名称：Original
- * 项目简介：
+* @file original.cpp
+ * @brief Original 库主文件
+ *
+ * @details
  * Original 是一个用于学习和实践的 C++ 库开发项目，目的是实现 STL 风格的模板类容器、算法以及相关工具。
  * 该库具有以下特点：
- * 1. 提供多种容器实现，包括 vector、chain、array 等，支持迭代器操作。
- * 2. 模板化设计，支持高性能泛型编程。
- * 3. 提供与标准库类似的接口，便于用户快速上手。
- * 4. 支持单元测试，确保代码的正确性和鲁棒性。
+ * - 提供多种容器实现，包括 vector、chain、array 等，支持迭代器操作。
+ * - 模板化设计，支持高性能泛型编程。
+ * - 提供与标准库类似的接口，便于用户快速上手。
+ * - 支持单元测试，确保代码的正确性和鲁棒性。
  *
  * @author FrozenLemonTee
- * GitHub地址：https://github.com/FrozenLemonTee/original
+ * @see GitHub 地址：https://github.com/FrozenLemonTee/original
  * @date 2024年10月
  *
  * @note 项目使用 CMake 构建，遵循现代 C++ 开发规范，支持跨平台构建。
  * @note 如需帮助，请参考项目文档或通过 GitHub 提交 issue。
  *
- * Project Name: Original
- * Project Introduction:
- * Original is a C++library development project for learning and practice, aimed at implementing STL style template class containers, algorithms, and related tools.
- * This library has the following characteristics:
- * 1.  Provide multiple container implementations, including vector, chain, array etc., supporting iterator operations.
- * 2.  Template based design, supporting high-performance generic programming.
- * 3.  Provide interfaces similar to standard libraries for users to quickly get started.
- * 4.  Support unit testing to ensure the correctness and robustness of the code.
+ * @par 项目名称
+ * Original
  *
- * @author FrozenLemonTee
- * GitHub address: https://github.com/FrozenLemonTee/original
- * @date October 2024
+ * @par 项目简介
+ * Original is a C++ library development project for learning and practice, aimed at implementing STL-style template class containers, algorithms, and related tools.
+ * This library has the following characteristics:
+ * - Provide multiple container implementations, including vector, chain, array, etc., supporting iterator operations.
+ * - Template-based design, supporting high-performance generic programming.
+ * - Provide interfaces similar to standard libraries for users to quickly get started.
+ * - Support unit testing to ensure the correctness and robustness of the code.
+ *
+ * @par 项目作者
+ * FrozenLemonTee
+ *
+ * @par GitHub 地址
+ * https://github.com/FrozenLemonTee/original
+ *
+ * @par 日期
+ * October 2024
  *
  * @note Project is built using CMake, following modern C++ development standards and supporting cross-platform construction.
  * @note If you need assistance, please refer to the project documentation or submit an issue through GitHub.
  */
 
-
 /**
- * Project header file included.
  * @headerfile original.h
+ * @brief Headers of the project included
  */
 #include "original.h"
