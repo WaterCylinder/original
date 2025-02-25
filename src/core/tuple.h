4a1639d0f9ebb626f4347f189cace202a096ab16
FrozenLemonTee <1115306170@qq.com>
2025-02-25 12:46:59 +0800

    feat: Add new methods get and set, remove redundant class, Use SFINAE in compareTo

--- src/core/tuple.h
+++ src/core/tuple.h
@@ -3,6 +3,7 @@
 
 #include "printable.h"
 #include "comparable.h"
+#include "types.h"
 
 
 namespace original{
@@ -11,30 +12,43 @@ namespace original{
         template<uint32_t I, typename... TS>
         class tupleImpl;
 
-        template<uint32_t I>
-        class tupleImpl<I> : public printable, public comparable<tupleImpl<I>>{
-            explicit tupleImpl() = default;
+        template<uint32_t I, typename T>
+        class tupleImpl<I, T> : public printable, public comparable<tupleImpl<I, T>>{
+            T cur_elem;
 
-            tupleImpl(const tupleImpl<I>& other) = default;
+            explicit tupleImpl(T cur);
 
+            tupleImpl(const tupleImpl<I, T>& other);
+
+            template<uint32_t I_DIFF>
+            auto get() const;
+
+            template<uint32_t I_DIFF, typename E>
+            void set(const E& e);
         public:
             friend class tuple;
-            int64_t compareTo(const tupleImpl<I>& other) const override;
+            int64_t compareTo(const tupleImpl<I, T>& other) const override;
 
             std::string toString(bool enter) const override;
         };
 
-        template<uint32_t I, typename T>
-        class tupleImpl<I, T> : public printable, public comparable<tupleImpl<I, T>>{
+        template<uint32_t I, typename T, typename TS>
+        class tupleImpl<I, T, TS> : public printable, public comparable<tupleImpl<I, T, TS>>{
             T cur_elem;
-            tupleImpl<I + 1> next;
+            tupleImpl<I + 1, TS> next;
 
-            explicit tupleImpl(T cur);
+            explicit tupleImpl(T cur, TS next_elems);
 
-            tupleImpl(const tupleImpl& other);
+            tupleImpl(const tupleImpl<I, T, TS>& other);
+
+            template<uint32_t I_DIFF>
+            auto get() const;
+
+            template<uint32_t I_DIFF, typename E>
+            void set(const E& e);
         public:
             friend class tuple;
-            int64_t compareTo(const tupleImpl<I, T>& other) const override;
+            int64_t compareTo(const tupleImpl<I, T, TS>& other) const override;
 
             std::string toString(bool enter) const override;
         };
@@ -46,7 +60,13 @@ namespace original{
 
             explicit tupleImpl(T cur, TS... next_elems);
 
-            tupleImpl(const tupleImpl& other);
+            tupleImpl(const tupleImpl<I, T, TS...>& other);
+
+            template<uint32_t I_DIFF>
+            auto get() const;
+
+            template<uint32_t I_DIFF, typename E>
+            void set(const E& e);
         public:
             friend class tuple;
             int64_t compareTo(const tupleImpl<I, T, TS...>& other) const override;
@@ -70,6 +90,12 @@ namespace original{
 
         tuple& operator=(tuple&& other) noexcept;
 
+        template<uint32_t IDX>
+        auto get() const;
+
+        template<uint32_t IDX, typename E>
+        void set(const E& e);
+
         int64_t compareTo(const tuple& other) const override;
 
         std::string toString(bool enter) const override;
@@ -79,40 +105,109 @@ namespace original{
 }
 
 template<typename... TYPES>
-template<uint32_t I>
-int64_t original::tuple<TYPES...>::tupleImpl<I>::compareTo(const tupleImpl<I>&) const {
-    return 0;
-}
-
-template<typename... TYPES>
-template<uint32_t I>
-std::string original::tuple<TYPES...>::tupleImpl<I>::toString(bool enter) const {
-    return "";
-}
-
-template<typename... TYPES>
 template<uint32_t I, typename T>
 original::tuple<TYPES...>::tupleImpl<I, T>::tupleImpl(T cur)
-    : cur_elem(cur), next() {}
+    : cur_elem(cur) {}
 
 template<typename... TYPES>
 template<uint32_t I, typename T>
 original::tuple<TYPES...>::tupleImpl<I, T>::tupleImpl(const tupleImpl<I, T>& other)
-    : cur_elem(other.cur_elem), next() {}
+    : cur_elem(other.cur_elem) {}
+
+template<typename... TYPES>
+template<uint32_t I, typename T>
+template<uint32_t I_DIFF>
+auto original::tuple<TYPES...>::tupleImpl<I, T>::get() const {
+    if constexpr (I_DIFF == 0){
+        return cur_elem;
+    }
+    throw outOfBoundError();
+}
+
+template<typename... TYPES>
+template<uint32_t I, typename T>
+template<uint32_t I_DIFF, typename E>
+void original::tuple<TYPES...>::tupleImpl<I, T>::set(const E& e) {
+    if constexpr (I_DIFF == 0 && std::same_as<T, E>){
+        cur_elem = e;
+    }
+    if constexpr (I_DIFF != 0)
+        throw outOfBoundError();
+    throw valueError();
+}
 
 template<typename... TYPES>
 template<uint32_t I, typename T>
 int64_t original::tuple<TYPES...>::tupleImpl<I, T>::compareTo(const tupleImpl<I, T>& other) const {
-    if (cur_elem != other.cur_elem)
-        return cur_elem < other.cur_elem ? -1 : 1;
-    return next.compareTo(other.next);
+    if constexpr (Comparable<T>){
+        if (cur_elem != other.cur_elem)
+            return cur_elem < other.cur_elem ? -1 : 1;
+    }
+    return 0;
 }
 
 template<typename... TYPES>
 template<uint32_t I, typename T>
 std::string original::tuple<TYPES...>::tupleImpl<I, T>::toString(bool enter) const {
     std::stringstream ss;
-    ss << ", " << cur_elem;
+    if constexpr (I != 0)
+        ss << ", ";
+    ss << cur_elem;
+    return ss.str();
+}
+
+template<typename... TYPES>
+template<uint32_t I, typename T, typename TS>
+original::tuple<TYPES...>::tupleImpl<I, T, TS>::tupleImpl(T cur, TS next_elems)
+    : cur_elem(cur), next(next_elems) {}
+
+template<typename... TYPES>
+template<uint32_t I, typename T, typename TS>
+original::tuple<TYPES...>::tupleImpl<I, T, TS>::tupleImpl(const tupleImpl<I, T, TS>& other)
+    : cur_elem(other.cur_elem), next(other.next) {}
+
+template<typename... TYPES>
+template<uint32_t I, typename T, typename TS>
+template<uint32_t I_DIFF>
+auto original::tuple<TYPES...>::tupleImpl<I, T, TS>::get() const {
+    if constexpr (I_DIFF == 0){
+        return cur_elem;
+    } else{
+        return next.template get<I_DIFF - 1>();
+    }
+}
+
+template<typename... TYPES>
+template<uint32_t I, typename T, typename TS>
+template<uint32_t I_DIFF, typename E>
+void original::tuple<TYPES...>::tupleImpl<I, T, TS>::set(const E& e) {
+    if constexpr (I_DIFF == 0){
+        if constexpr (!std::same_as<T, E>)
+            throw valueError();
+        cur_elem = e;
+        return;
+    }
+    next.template set<I_DIFF - 1, E>(e);
+}
+
+template<typename... TYPES>
+template<uint32_t I, typename T, typename TS>
+int64_t
+original::tuple<TYPES...>::tupleImpl<I, T, TS>::compareTo(const tupleImpl<I, T, TS>& other) const {
+    if constexpr (Comparable<T>){
+        if (cur_elem != other.cur_elem)
+            return cur_elem < other.cur_elem ? -1 : 1;
+    }
+    return next.compareTo(other.next);
+}
+
+template<typename... TYPES>
+template<uint32_t I, typename T, typename TS>
+std::string original::tuple<TYPES...>::tupleImpl<I, T, TS>::toString(bool enter) const {
+    std::stringstream ss;
+    if constexpr (I != 0)
+        ss << ", ";
+    ss << cur_elem;
     ss << next;
     return ss.str();
 }
@@ -129,10 +224,36 @@ original::tuple<TYPES...>::tupleImpl<I, T, TS...>::tupleImpl(const tupleImpl<I, 
 
 template<typename... TYPES>
 template<uint32_t I, typename T, typename... TS>
+template<uint32_t I_DIFF>
+auto original::tuple<TYPES...>::tupleImpl<I, T, TS...>::get() const {
+    if constexpr (I_DIFF == 0){
+        return cur_elem;
+    } else{
+        return next.template get<I_DIFF - 1>();
+    }
+}
+
+template<typename... TYPES>
+template<uint32_t I, typename T, typename... TS>
+template<uint32_t I_DIFF, typename E>
+void original::tuple<TYPES...>::tupleImpl<I, T, TS...>::set(const E& e) {
+    if constexpr (I_DIFF == 0){
+        if constexpr (!std::same_as<T, E>)
+            throw valueError();
+        cur_elem = e;
+        return;
+    }
+    next.template set<I_DIFF - 1, E>(e);
+}
+
+template<typename... TYPES>
+template<uint32_t I, typename T, typename... TS>
 int64_t
 original::tuple<TYPES...>::tupleImpl<I, T, TS...>::compareTo(const tupleImpl<I, T, TS...>& other) const {
-    if (cur_elem != other.cur_elem)
-        return cur_elem < other.cur_elem ? -1 : 1;
+    if constexpr (Comparable<T>){
+        if (cur_elem != other.cur_elem)
+            return cur_elem < other.cur_elem ? -1 : 1;
+    }
     return next.compareTo(other.next);
 }
 
@@ -172,6 +293,18 @@ original::tuple<TYPES...>& original::tuple<TYPES...>::operator=(tuple&& other) n
     if (this == &other) return *this;
     this->elems = std::move(other.elems);
     return *this;
+}
+
+template<typename... TYPES>
+template<uint32_t IDX>
+auto original::tuple<TYPES...>::get() const {
+    return elems.template get<IDX>();
+}
+
+template<typename... TYPES>
+template<uint32_t IDX, typename E>
+void original::tuple<TYPES...>::set(const E &e) {
+    elems.template set<IDX, E>(e);
 }
 
 template<typename... TYPES>
