c1895467e8dab63c7a0050d9a0e7c832516e1d12
FrozenLemonTee <1115306170@qq.com>
2025-04-07 23:11:48 +0800

    feat: Add fnv1a and more hashFunc

--- src/core/hash.h
+++ src/core/hash.h
@@ -3,6 +3,7 @@
 
 
 #include "config.h"
+#include <cstring>
 #include "types.h"
 
 
@@ -14,17 +15,36 @@ namespace original {
     class hashable;
 
     template <typename DERIVED>
-    concept isHashable = ExtendsOf<hashable<DERIVED>, DERIVED>;
+    concept isHashable =
+        requires(const DERIVED& t, const DERIVED& other) {
+            { t.toHash() } -> std::same_as<original::u_integer>;
+            { t.equals(other) } -> std::same_as<bool>;
+        };
 
 
     template <typename TYPE>
     class hash {
+    public:
+        static constexpr u_integer FNV_OFFSET_BASIS = 0x811C9DC5;
+
+        static constexpr u_integer FNV_32_PRIME = 0x01000193;
+
+        static u_integer fnv1a(const byte* data, u_integer size) noexcept;
+
         template <typename T>
         static u_integer hashFunc(const T& t) noexcept;
 
         template <isHashable T>
         static u_integer hashFunc(const T& t) noexcept;
-    public:
+
+        template<std::integral T>
+        static u_integer hashFunc(const T& t) noexcept;
+
+        template <typename T>
+        static u_integer hashFunc(T* const& t) noexcept;
+
+        static u_integer hashFunc(const char& t) noexcept;
+
         u_integer operator()(const TYPE& t) noexcept;
     };
 
@@ -40,15 +60,48 @@ namespace original {
 }
 
 template<typename TYPE>
+original::u_integer original::hash<TYPE>::fnv1a(const byte* data, u_integer size) noexcept {
+    u_integer hash = FNV_OFFSET_BASIS;
+    for (u_integer i = 0; i < size; ++i) {
+        hash ^= data[i];
+        hash *= FNV_32_PRIME;
+    }
+    return hash;
+}
+
+template<typename TYPE>
 template<typename T>
 original::u_integer original::hash<TYPE>::hashFunc(const T &t) noexcept {
-    return static_cast<u_integer>(t);
+    if constexpr (std::is_trivially_copyable_v<T>) {
+        byte buffer[sizeof(T)];
+        std::memcpy(buffer, &t, sizeof(T));
+        return fnv1a(buffer, sizeof(T));
+    } else {
+        return static_cast<u_integer>(&t);
+    }
 }
 
 template<typename TYPE>
 template<original::isHashable T>
 original::u_integer original::hash<TYPE>::hashFunc(const T &t) noexcept {
     return t.toHash();
+}
+
+template<typename TYPE>
+template<std::integral T>
+original::u_integer original::hash<TYPE>::hashFunc(const T &t) noexcept {
+    return static_cast<u_integer>(t);
+}
+
+template<typename TYPE>
+template<typename T>
+original::u_integer original::hash<TYPE>::hashFunc(T* const& t) noexcept {
+    return static_cast<u_integer>(t);
+}
+
+template<typename TYPE>
+original::u_integer original::hash<TYPE>::hashFunc(const char &t) noexcept {
+    return static_cast<u_integer>(t);
 }
 
 template<typename TYPE>
