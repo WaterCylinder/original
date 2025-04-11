3d56c39c158d0a60af704fcf7e02b723a6f18622
FrozenLemonTee <1115306170@qq.com>
2025-04-11 18:15:10 +0800

    feat: HashTable is almost complete

--- src/core/hashTable.h
+++ src/core/hashTable.h
@@ -53,6 +53,19 @@ namespace original {
         using rebind_alloc_node = typename ALLOC::template rebind_alloc<hashNode>;
         using rebind_alloc_pointer = typename ALLOC::template rebind_alloc<hashNode*>;
         static constexpr u_integer BUCKETS_SIZE_INIT = 16;
+        static constexpr floating LOAD_FACTOR_MIN = 0.25;
+        static constexpr floating LOAD_FACTOR_MAX = 0.75;
+        static constexpr u_integer BUCKETS_SIZES_COUNT = 30;
+        static constexpr u_integer BUCKETS_SIZES[] = {
+                17,          29,          53,          97,          193,
+                389,         769,         1543,        3079,        6151,
+                12289,       24593,       49157,       98317,       196613,
+
+                393241,      786433,      1572869,     3145739,     6291469,
+                12582917,    25165843,    50331653,    100663319,   201326611,
+
+                402653189,   805306457,   1610612741,  3221225473,  4294967291
+        };
 
         u_integer size_;
         vector<hashNode*, rebind_alloc_pointer> buckets;
@@ -71,6 +84,14 @@ namespace original {
         hashNode* getBucket(const K_TYPE& key) const;
 
         floating loadFactor() const;
+
+        u_integer getNextSize() const;
+
+        u_integer getPrevSize() const;
+
+        void rehash(u_integer new_bucket_count);
+
+        void adjust();
     protected:
 
         explicit hashTable(HASH hash = HASH{});
@@ -207,6 +228,56 @@ original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::loadFactor() const {
 }
 
 template<typename K_TYPE, typename V_TYPE, typename ALLOC, typename HASH>
+original::u_integer original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::getNextSize() const {
+    for (u_integer i : BUCKETS_SIZES){
+        if (this->getBucketCount() < i){
+            return i;
+        }
+    }
+    throw outOfBoundError();
+}
+
+template<typename K_TYPE, typename V_TYPE, typename ALLOC, typename HASH>
+original::u_integer original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::getPrevSize() const {
+    u_integer current = this->getBucketCount();
+    for (u_integer i = BUCKETS_SIZES_COUNT - 1; i > 0; --i){
+        if (BUCKETS_SIZES[i] < current){
+            return BUCKETS_SIZES[i];
+        }
+    }
+    return BUCKETS_SIZES[0];
+}
+
+template<typename K_TYPE, typename V_TYPE, typename ALLOC, typename HASH>
+void original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::rehash(original::u_integer new_bucket_count) {
+    if (new_bucket_count == this->getBucketCount())
+        return;
+
+    auto new_buckets = vector<hashNode*, rebind_alloc_pointer>(new_bucket_count, rebind_alloc_pointer{}, nullptr);
+    for (hashNode*& bucket: this->buckets){
+        while (bucket){
+            auto next = bucket->getPNext();
+            auto cur = bucket;
+            hashNode::connect(bucket, next);
+            hashNode::connect(cur, nullptr);
+            auto code = this->hash_(cur->getKey()) % new_bucket_count;
+            hashNode::connect(cur, new_buckets[code]);
+            new_buckets[code] = cur;
+        }
+    }
+    this->buckets = std::move(new_buckets);
+}
+
+template<typename K_TYPE, typename V_TYPE, typename ALLOC, typename HASH>
+void original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::adjust() {
+    if (this->loadFactor() <= LOAD_FACTOR_MIN){
+        this->rehash(this->getPrevSize());
+    } else if (this->loadFactor() >= LOAD_FACTOR_MAX){
+        this->rehash(this->getNextSize());
+    }
+}
+
+template<typename K_TYPE, typename V_TYPE, typename ALLOC, typename HASH>
 original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::hashTable(HASH hash)
     : size_(0), hash_(std::move(hash)) {
     this->buckets = vector<hashNode*, rebind_alloc_pointer>(BUCKETS_SIZE_INIT, rebind_alloc_pointer{}, nullptr);
@@ -218,7 +289,7 @@ original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::find(const K_TYPE& key) const 
     if (this->size_ == 0)
         return nullptr;
 
-    for (auto cur = this->getBucket(key); cur; cur = cur->next_){
+    for (auto cur = this->getBucket(key); cur; cur = cur->getPNext()){
         if (cur->getKey() == key)
             return cur;
     }
@@ -238,8 +309,9 @@ bool original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::modify(const K_TYPE &key,
 
 template<typename K_TYPE, typename V_TYPE, typename ALLOC, typename HASH>
 bool original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::insert(const K_TYPE &key, const V_TYPE &value) {
-    auto cur = this->getBucket(key);
+    this->adjust();
 
+    auto cur = this->getBucket(key);
     if (!cur){
         this->buckets[this->getHashCode(key)] = this->createNode(key, value);
     } else{
@@ -247,7 +319,7 @@ bool original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::insert(const K_TYPE &key,
             if (cur->getKey() == key)
                 return false;
         }
-        cur->setPNext(this->createNode(key, value));
+        hashNode::connect(cur, this->createNode(key, value));
     }
 
     this->size_ += 1;
@@ -259,6 +331,8 @@ bool original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::erase(const K_TYPE &key) 
     if (this->size_ == 0)
         return false;
 
+    this->adjust();
+
     u_integer code = this->getHashCode(key);
     hashNode* cur = this->buckets[code];
     hashNode* prev = nullptr;
@@ -266,7 +340,7 @@ bool original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::erase(const K_TYPE &key) 
     while (cur){
         if (cur->getKey() == key) {
             if (prev) {
-                prev->setPNext(cur->getPNext());
+                hashNode::connect(prev, cur->getPNext());
             } else {
                 this->buckets[code] = cur->getPNext();
             }
@@ -288,7 +362,7 @@ std::string original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::className() const 
 
 template<typename K_TYPE, typename V_TYPE, typename ALLOC, typename HASH>
 original::hashTable<K_TYPE, V_TYPE, ALLOC, HASH>::~hashTable() {
-    for (auto bucket: this->buckets) {
+    for (hashNode*& bucket: this->buckets) {
         while (bucket){
             auto next = bucket->getPNext();
             this->destroyNode(bucket);
