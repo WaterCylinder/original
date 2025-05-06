f49ef4d7276e3676400111cccabd0853f1b4861f
FrozenLemonTee <1115306170@qq.com>
2025-05-06 19:56:41 +0800

    ci: Add generator argument

--- install.cmake
+++ install.cmake
@@ -4,10 +4,15 @@ set(SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}")
 set(BUILD_DIR "${SOURCE_DIR}/cmake-build-debug/build")
 set(INSTALL_DIR "${SOURCE_DIR}/original")
 
+if(NOT DEFINED GENERATOR)
+    set(GENERATOR "MinGW Makefiles")
+endif()
+
 execute_process(
         COMMAND ${CMAKE_COMMAND}
         -S ${SOURCE_DIR}
         -B ${BUILD_DIR}
+        -G ${GENERATOR}
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
         -DBUILD_TESTING=OFF
