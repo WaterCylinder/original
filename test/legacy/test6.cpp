f04d6788d9a69a5122d7e38f51c5ad1560a242b8
frozenlemontee <frozenlemontee@gmail.com>
2025-05-17 21:16:11 +0800

    test: Add tests for pThread

---
+++ test/legacy/test6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "thread.h"
+
+int main()
+{
+    auto task1 = [](const int a, const std::string& b)
+    {
+        std::cout << b << a << std::endl;
+    };
+    original::pThread t1{task1, 1, "show: "};
+    t1.join();
+    original::pThread t2{task1, 2, "show: "};
+    t2.join();
+    original::pThread t3{task1, 3, "show: "};
+    auto t4 = std::move(t3);
+    t4.join();
+
+    original::pThread t5{task1, 4, "show: "};
+    original::pThread t6{task1, 5, "show: "};
+    t6 = std::move(t5);
+    t6.join();
+    return 0;
+}
