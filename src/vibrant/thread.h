7ec60a5870d4c15f493fa1c76f3640b46519295e
frozenlemontee <frozenlemontee@gmail.com>
2025-05-19 15:31:39 +0800

    feat: Add thread

--- src/vibrant/thread.h
+++ src/vibrant/thread.h
@@ -25,9 +25,9 @@ namespace original {
         [[nodiscard]] virtual bool valid() const = 0;
 
     public:
-        explicit threadBase();
+        explicit threadBase(bool is_joinable = false);
 
-        virtual ~threadBase();
+        virtual ~threadBase() noexcept(false);
 
         threadBase(const threadBase&) = delete;
         threadBase& operator=(const threadBase&) = delete;
@@ -39,6 +39,8 @@ namespace original {
         explicit operator bool() const;
 
         bool operator!() const;
+
+        [[nodiscard]] bool joinable() const;
     };
 
     class pThread final : public threadBase {
@@ -46,6 +48,8 @@ namespace original {
 
         [[nodiscard]] bool valid() const override;
     public:
+        explicit pThread();
+
         template<typename Callback, typename... ARGS>
         explicit pThread(Callback c, ARGS&&... args);
 
@@ -56,6 +60,37 @@ namespace original {
         void join();
 
         void detach();
+    };
+
+    class thread {
+
+        pThread thread_;
+        bool will_join;
+    public:
+        explicit thread();
+
+        template<typename Callback, typename... ARGS>
+        explicit thread(Callback c, ARGS&&... args);
+
+        template<typename Callback, typename... ARGS>
+        explicit thread(Callback c, bool will_join, ARGS&&... args);
+
+        explicit thread(pThread p_thread, bool will_join = true);
+
+        thread(const thread&) = delete;
+        thread& operator=(const thread&) = delete;
+
+        thread(thread&& other) noexcept;
+
+        thread(thread&& other, bool will_join) noexcept;
+
+        thread& operator=(thread&& other) noexcept;
+
+        void join();
+
+        void detach();
+
+        ~thread();
     };
 }
 
@@ -86,25 +121,35 @@ inline bool original::threadBase::operator!() const
     return !this->valid();
 }
 
-inline original::threadBase::threadBase()
-    : is_joinable(true) {}
+inline bool original::threadBase::joinable() const {
+    return this->is_joinable;
+}
 
-inline original::threadBase::~threadBase() {
+inline original::threadBase::threadBase(bool is_joinable)
+    : is_joinable(is_joinable) {}
+
+inline original::threadBase::~threadBase() noexcept(false) {
     if (this->is_joinable) {
         throw sysError();
     }
 }
 
+inline original::pThread::pThread() : handle() {}
+
 template<typename Callback, typename... ARGS>
-original::pThread::pThread(Callback c, ARGS&&... args) : handle()
+original::pThread::pThread(Callback c, ARGS&&... args) : threadBase(true), handle()
 {
-    using bound_callback = decltype(std::bind(std::forward<Callback>(c), std::forward<ARGS>(args)...));
+    auto bound_lambda =
+    [func = std::forward<Callback>(c), ...lambda_args = std::forward<ARGS>(args)]() mutable {
+        std::invoke(std::move(func), std::move(lambda_args)...);
+    };
+
+    using bound_callback = decltype(bound_lambda);
     using bound_thread_data = threadData<bound_callback>;
 
-    auto task = new threadData<bound_callback>(std::bind(std::forward<Callback>(c), std::forward<ARGS>(args)...));
+    auto task = new bound_thread_data(std::move(bound_lambda));
 
-    if (const int code = pthread_create(&this->handle, nullptr, &bound_thread_data::run, task);
-        code != 0)
+    if (const int code = pthread_create(&this->handle, nullptr, &bound_thread_data::run, task); code != 0)
     {
         throw sysError();
     }
@@ -154,6 +199,54 @@ inline void original::pThread::detach() {
             throw sysError();
         }
         this->is_joinable = false;
+    }
+}
+
+inline original::thread::thread()
+    : will_join(true) {}
+
+template <typename Callback, typename ... ARGS>
+original::thread::thread(Callback c, ARGS&&... args)
+    : thread_(std::forward<Callback>(c), std::forward<ARGS>(args)...), will_join(true) {}
+
+template <typename Callback, typename ... ARGS>
+original::thread::thread(Callback c, const bool will_join, ARGS&&... args)
+    : thread_(std::forward<Callback>(c), std::forward<ARGS>(args)...), will_join(will_join) {}
+
+inline original::thread::thread(pThread p_thread, const bool will_join)
+    : thread_(std::move(p_thread)), will_join(will_join) {}
+
+inline original::thread::thread(thread&& other) noexcept
+    : thread_(std::move(other.thread_)), will_join(true) {}
+
+inline original::thread::thread(thread&& other, const bool will_join) noexcept
+    : thread_(std::move(other.thread_)), will_join(will_join) {}
+
+inline original::thread& original::thread::operator=(thread&& other) noexcept {
+    if (this == &other) {
+        return *this;
+    }
+
+    this->thread_ = std::move(other.thread_);
+    this->will_join = other.will_join;
+    other.will_join = false;
+    return *this;
+}
+
+inline void original::thread::join()
+{
+    this->thread_.join();
+}
+
+inline void original::thread::detach()
+{
+    this->thread_.detach();
+}
+
+inline original::thread::~thread()
+{
+    if (this->thread_.joinable()) {
+        this->will_join ? this->thread_.join() : this->thread_.detach();
     }
 }
 
