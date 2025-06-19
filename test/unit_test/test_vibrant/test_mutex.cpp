147d15bd3d760470d8d96e0c3cec639f8263f898
FrozenLemonTee <1115306170@qq.com>
2025-06-19 08:00:41 +0800

    test: Add more tests for scopeLock

--- test/unit_test/test_vibrant/test_mutex.cpp
+++ test/unit_test/test_vibrant/test_mutex.cpp
@@ -1,22 +1,23 @@
 #include <gtest/gtest.h>
 #include <thread>
 #include "mutex.h"
+#include "thread.h"
 
 using namespace original;
 
-TEST(pMutexTest, LockUnlockDoesNotThrow) {
+TEST(MutexTest, LockUnlockDoesNotThrow) {
     pMutex m;
     EXPECT_NO_THROW(m.lock());
     EXPECT_NO_THROW(m.unlock());
 }
 
-TEST(pMutexTest, TryLockSuccess) {
+TEST(MutexTest, TryLockSuccess) {
     pMutex m;
     EXPECT_NO_THROW(m.tryLock());
     EXPECT_NO_THROW(m.unlock());
 }
 
-TEST(pMutexTest, TryLockContested) {
+TEST(MutexTest, TryLockContested) {
     pMutex m;
     std::thread t(
         [&m]() {
@@ -28,11 +29,11 @@ TEST(pMutexTest, TryLockContested) {
 
     std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 确保 t 抢先上锁
 
-    EXPECT_THROW(m.tryLock(), sysError);  // 此时 tryLock 应该失败
+    EXPECT_FALSE(m.tryLock());  // 此时 tryLock 应该失败
     t.join();
 }
 
-TEST(pMutexTest, PreventsDataRaceWithMultipleThreads) {
+TEST(MutexTest, PreventsDataRaceWithMultipleThreads) {
     constexpr int thread_count = 10;
     constexpr int iterations = 10000;
     int counter = 0;
@@ -59,28 +60,108 @@ TEST(pMutexTest, PreventsDataRaceWithMultipleThreads) {
 }
 
 TEST(MutexTest, LockInConstructorAndUnlockInDestructor) {
+    pMutex pm;
     {
-        mutex m;  // 默认锁定
-        // 这里无法直接验证锁状态，但至少应无异常
+        scopeLock m(pm);  // 默认锁定
+        EXPECT_TRUE(m.isLocked());
     }
     SUCCEED();
 }
 
 TEST(MutexTest, TryLockConstructor) {
+    pMutex pm;
     {
-        mutex m(true);  // 使用 tryLock
-        // tryLock 失败会抛出异常
+        scopeLock m(pm, true);  // 使用 tryLock
+        EXPECT_TRUE(m.isLocked());
     }
     SUCCEED();
 }
 
 TEST(MutexTest, MutexIsNonCopyable) {
-    EXPECT_FALSE(std::is_copy_constructible<mutex>::value);
-    EXPECT_FALSE(std::is_copy_assignable<mutex>::value);
+    EXPECT_FALSE(std::is_copy_constructible<scopeLock>::value);
+    EXPECT_FALSE(std::is_copy_assignable<scopeLock>::value);
 }
 
 TEST(MutexTest, MutexIsNonMovable) {
-    EXPECT_FALSE(std::is_move_constructible<mutex>::value);
-    EXPECT_FALSE(std::is_move_assignable<mutex>::value);
+    EXPECT_FALSE(std::is_move_constructible<scopeLock>::value);
+    EXPECT_FALSE(std::is_move_assignable<scopeLock>::value);
 }
 
+// TryLock失败时，scopeLock::isLocked() 应为 false
+TEST(MutexTest, TryLockFailsIsLockedFalse) {
+    pMutex pm;
+    pm.lock();  // 主线程先锁住
+
+    scopeLock s(pm, true);  // 尝试获取失败
+    EXPECT_FALSE(s.isLocked());
+
+    pm.unlock();  // 主线程释放锁
+}
+
+// scopeLock析构后，mutex可以再次上锁
+TEST(MutexTest, RAIIUnlocksCorrectly) {
+    pMutex pm;
+    {
+        scopeLock s(pm);
+        EXPECT_TRUE(s.isLocked());
+    }
+
+    // 如果前面未释放成功，这里会阻塞或抛异常
+    EXPECT_NO_THROW(pm.lock());
+    pm.unlock();
+}
+
+// 多线程验证scopeLock作用域内有效
+TEST(MutexTest, ScopeLockProtectsCriticalSection) {
+    constexpr int thread_count = 10;
+    constexpr int iterations = 10000;
+    int counter = 0;
+    pMutex m;
+
+    auto increment = [&]() {
+        for (int i = 0; i < iterations; ++i) {
+            scopeLock lock(m);
+            ++counter;
+        }
+    };
+
+    std::vector<thread> threads;
+    for (int i = 0; i < thread_count; ++i) {
+        threads.emplace_back(increment);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    EXPECT_EQ(counter, thread_count * iterations);
+}
+
+// TryLock获取失败时不会 unlock
+TEST(MutexTest, TryLockFailDoesNotUnlock) {
+    pMutex pm;
+    pm.lock();
+
+    {
+        scopeLock s(pm, true);
+        EXPECT_FALSE(s.isLocked());
+    }
+
+    // pm 依旧被主线程持有，解锁后测试结束
+    EXPECT_NO_THROW(pm.unlock());
+}
+
+// TryLock获取成功后，锁应被释放
+TEST(MutexTest, TryLockSuccessUnlocksOnDestruction) {
+    pMutex pm;
+    {
+        scopeLock s(pm, true);
+        if (s.isLocked()) {
+            SUCCEED();  // 成功加锁
+        }
+    }
+
+    // 若未正确释放，此处将阻塞
+    EXPECT_NO_THROW(pm.lock());
+    pm.unlock();
+}
