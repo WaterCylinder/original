87359d2f37d9ba2a0acdf66a1694db73fa3b572d
FrozenLemonTee <1115306170@qq.com>
2025-06-27 15:57:14 +0800

    feat: Add class time::point

--- src/vibrant/zeit.h
+++ src/vibrant/zeit.h
@@ -55,6 +55,8 @@ namespace original {
             time_val_type nano_seconds_;
 
         public:
+            friend class point;
+
             static const duration ZERO;
 
             explicit duration(time_val_type val = 0, unit unit = MILLISECOND);
@@ -101,6 +103,38 @@ namespace original {
 
             friend duration abs(const duration& d);
         };
+
+        class point final
+                : public comparable<point>,
+                  public hashable<point>,
+                  public printable {
+            duration nano_since_epoch_;
+
+        public:
+            static point now();
+
+            explicit point(time_val_type val = 0, unit unit = MILLISECOND);
+
+            explicit point(duration  d);
+
+            [[nodiscard]] time_val_type value(unit unit = MILLISECOND) const noexcept;
+
+            [[nodiscard]] integer compareTo(const point& other) const override;
+
+            u_integer toHash() const noexcept override;
+
+            std::string toString(bool enter) const override;
+
+            point& operator+=(const duration& d);
+
+            point& operator-=(const duration& d);
+
+            friend point operator+(const point& p, const duration& d);
+
+            friend point operator-(const point& p, const duration& d);
+
+            friend duration operator-(const point& lhs, const point& rhs);
+        };
     };
 
     const time::duration original::time::duration::ZERO = duration{};
@@ -120,6 +154,12 @@ namespace original {
     time::duration operator/(const time::duration& lhs, const time::duration& rhs);
 
     time::duration abs(const time::duration& d);
+
+    time::point operator+(const time::point& p, const time::duration& d);
+
+    time::point operator-(const time::point& p, const time::duration& d);
+
+    time::duration operator-(const time::point& lhs, const time::point& rhs);
 
     namespace literals {
         inline time::duration operator"" _ns(unsigned long long val) {
@@ -348,6 +388,84 @@ original::abs(const time::duration& d) {
     time::time_val_type val = d.nano_seconds_;
     val = val < 0 ? -val : val;
     return time::duration{val, time::NANOSECOND};
+}
+
+original::time::point original::time::point::now() {
+#if ORIGINAL_COMPILER_GCC || (ORIGINAL_COMPILER_CLANG && ORIGINAL_PLATFORM_LINUX)
+    struct timespec ts{};
+    clock_gettime(CLOCK_REALTIME, &ts);
+    time_val_type ns = ts.tv_sec * FACTOR_SECOND + ts.tv_nsec;
+    return point(ns, NANOSECOND);
+#elif ORIGINAL_PLATFORM_APPLE
+    struct timeval tv;
+    gettimeofday(&tv, nullptr);
+    time_val_type ns = tv.tv_sec * FACTOR_SECOND + tv.tv_usec * 1000;
+    return point(ns, NANOSECOND);
+#else
+    // Other implements not complete
+    return point();
+#endif
+}
+
+original::time::point::point(time::time_val_type val, time::unit unit)
+    : nano_since_epoch_(val, unit) {}
+
+original::time::point::point(time::duration d)
+    : nano_since_epoch_(std::move(d)) {}
+
+original::time::time_val_type
+original::time::point::value(time::unit unit) const noexcept {
+    return this->nano_since_epoch_.value(unit);
+}
+
+original::integer
+original::time::point::compareTo(const time::point& other) const {
+    return this->nano_since_epoch_.compareTo(other.nano_since_epoch_);
+}
+
+original::u_integer
+original::time::point::toHash() const noexcept {
+    return this->nano_since_epoch_.toHash();
+}
+
+std::string
+original::time::point::toString(bool enter) const {
+    std::stringstream ss;
+    ss << "(point " << this->nano_since_epoch_.nano_seconds_ << ")";
+    if (enter)
+        ss << "\n";
+    return ss.str();
+}
+
+original::time::point&
+original::time::point::operator+=(const time::duration& d) {
+    this->nano_since_epoch_ += d;
+    return *this;
+}
+
+original::time::point&
+original::time::point::operator-=(const time::duration& d) {
+    this->nano_since_epoch_ -= d;
+    return *this;
+}
+
+original::time::point
+original::operator-(const time::point &p, const time::duration &d) {
+    time::point res(p);
+    res -= d;
+    return res;
+}
+
+original::time::duration
+original::operator-(const time::point &lhs, const time::point &rhs) {
+    return lhs.nano_since_epoch_ - rhs.nano_since_epoch_;
+}
+
+original::time::point
+original::operator+(const time::point &p, const time::duration &d) {
+    time::point res(p);
+    res += d;
+    return res;
 }
 
 #endif //ORIGINAL_ZEIT_H
