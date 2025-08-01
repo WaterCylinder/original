970954b99e161d5f888a2bb67038f5d391e3c1ea
frozenlemontee <frozenlemontee@gmail.com>
2025-08-01 16:34:51 +0800

    test: Simple test for async

---
+++ test/other/test7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "async.h"
+
+
+int main() {
+    auto simple_func = []{
+        original::thread::sleep(original::seconds(1));
+        return 0;
+    };
+
+    auto add_func = [](const int a, const int b){
+        original::thread::sleep(original::seconds(1));
+        return a + b;
+    };
+
+    const original::async::promise<int, int()> p {simple_func};
+    const auto f = p.getFuture();
+    std::cout << f.result() << std::endl;
+
+    std::cout << original::async::get<int, int()>(simple_func) << std::endl;
+    std::cout << original::async::get<int, int(int, int)>(add_func, 1, 5) << std::endl;
+    return 0;
+}
