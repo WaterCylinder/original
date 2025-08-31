1560c4b5ca66a90db1181ebb7b8a3b11d32558d5
FrozenlemonTee <1115306170@qq.com>
2025-08-31 20:33:10 +0800

    refactor: Use template specialization to split lock and non-lock strategies

--- src/vibrant/atomic.h
+++ src/vibrant/atomic.h
@@ -1,119 +1,172 @@
 #ifndef ORIGINAL_ATOMIC_H
 #define ORIGINAL_ATOMIC_H
+
+#include <type_traits>
 #include <cstring>
+#include "optional.h"
 #include "config.h"
 #include "mutex.h"
 
 namespace original {
 #ifdef ORIGINAL_COMPILER_GCC
-    template<typename TYPE>
-    class atomic {
-        static constexpr bool USE_LOCK_FREE = __atomic_always_lock_free(sizeof(TYPE), nullptr);
-        #define ORIGINAL_USE_LOCK !USE_LOCK_FREE
 
-        enum class memOrder {
-            RELAXED = __ATOMIC_RELAXED,
-            ACQUIRE = __ATOMIC_ACQUIRE,
-            RELEASE = __ATOMIC_RELEASE,
-            ACQ_REL = __ATOMIC_ACQ_REL,
-            SEQ_CST = __ATOMIC_SEQ_CST,
-        };
-
-    #ifdef ORIGINAL_USE_LOCK
-        mutable pMutex mutex_;
-    #endif
-        alignas(TYPE) byte data_[sizeof(TYPE)];
-
-        public:
-            static constexpr auto RELAXED = memOrder::RELAXED;
-            static constexpr auto ACQUIRE = memOrder::ACQUIRE;
-            static constexpr auto RELEASE = memOrder::RELEASE;
-            static constexpr auto ACQ_REL = memOrder::ACQ_REL;
-            static constexpr auto SEQ_CST = memOrder::SEQ_CST;
-
-            atomic(const atomic&) noexcept = delete;
-
-            atomic& operator=(const atomic&) noexcept = delete;
-
-            atomic(atomic&&) noexcept = delete;
-
-            atomic& operator=(atomic&&) noexcept = delete;
-
-            explicit atomic();
-
-            explicit atomic(TYPE value, memOrder order = RELEASE);
-
-            static constexpr bool isLockFree() noexcept;
-
-            void store(TYPE value, memOrder order = RELEASE);
-
-            TYPE load(memOrder order = ACQUIRE) const noexcept;
-
-            TYPE exchange(const TYPE& value, memOrder order = ACQUIRE) noexcept;
-
-            ~atomic() = default;
+    enum class memOrder {
+        RELAXED = __ATOMIC_RELAXED,
+        ACQUIRE = __ATOMIC_ACQUIRE,
+        RELEASE = __ATOMIC_RELEASE,
+        ACQ_REL = __ATOMIC_ACQ_REL,
+        SEQ_CST = __ATOMIC_SEQ_CST,
     };
+
+    template<typename TYPE, bool USE_LOCK>
+    class atomicImpl;
+
+    template<typename TYPE>
+    class atomicImpl<TYPE, false> {
+        alignas(TYPE) unsigned char data_[sizeof(TYPE)];
+
+    public:
+        static constexpr auto RELAXED = memOrder::RELAXED;
+        static constexpr auto ACQUIRE = memOrder::ACQUIRE;
+        static constexpr auto RELEASE = memOrder::RELEASE;
+        static constexpr auto ACQ_REL = memOrder::ACQ_REL;
+        static constexpr auto SEQ_CST = memOrder::SEQ_CST;
+
+        atomicImpl();
+
+        explicit atomicImpl(TYPE value, memOrder order = RELEASE);
+
+        static constexpr bool isLockFree() noexcept;
+
+        void store(TYPE value, memOrder order = RELEASE);
+
+        TYPE load(memOrder order = ACQUIRE) const noexcept;
+
+        TYPE exchange(const TYPE& value, memOrder order = ACQ_REL) noexcept;
+
+        ~atomicImpl() = default;
+    };
+
+    template<typename TYPE>
+    class atomicImpl<TYPE, true> {
+        mutable pMutex mutex_;
+        alternative<TYPE> data_;
+
+    public:
+        static constexpr auto RELAXED = memOrder::RELAXED;
+        static constexpr auto ACQUIRE = memOrder::ACQUIRE;
+        static constexpr auto RELEASE = memOrder::RELEASE;
+        static constexpr auto ACQ_REL = memOrder::ACQ_REL;
+        static constexpr auto SEQ_CST = memOrder::SEQ_CST;
+
+        atomicImpl() = default;
+
+        explicit atomicImpl(TYPE value, memOrder = RELEASE);
+
+        static constexpr bool isLockFree() noexcept;
+
+        void store(TYPE value, memOrder = RELEASE);
+
+        TYPE load(memOrder = ACQUIRE) const noexcept;
+
+        TYPE exchange(const TYPE& value, memOrder = ACQ_REL) noexcept;
+
+        ~atomicImpl() = default;
+    };
+
+    template<typename TYPE>
+    auto atomic();
+
+    template<typename TYPE>
+    auto atomic(TYPE value);
 }
 
 template <typename TYPE>
-original::atomic<TYPE>::atomic() : data_{} {}
-
-template <typename TYPE>
-original::atomic<TYPE>::atomic(TYPE value, memOrder order) : atomic()
-{
-    if constexpr (ORIGINAL_USE_LOCK) {
-        uniqueLock lock{this->mutex_};
-        std::memcpy(this->data_, &value, sizeof(TYPE));
-    } else {
-        __atomic_store(this->data_, &value, static_cast<integer>(order));
-    }
+original::atomicImpl<TYPE, false>::atomicImpl() {
+    std::memset(this->data_, 0, sizeof(TYPE));
 }
 
 template <typename TYPE>
-constexpr bool original::atomic<TYPE>::isLockFree() noexcept
-{
-    return USE_LOCK_FREE;
+original::atomicImpl<TYPE, false>::atomicImpl(TYPE value, memOrder order) {
+    __atomic_store(reinterpret_cast<TYPE*>(this->data_), &value, static_cast<int>(order));
 }
 
 template <typename TYPE>
-void original::atomic<TYPE>::store(TYPE value, memOrder order)
-{
-    if constexpr (ORIGINAL_USE_LOCK) {
-        uniqueLock lock{this->mutex_};
-        std::memcpy(this->data_, &value, sizeof(TYPE));
-    } else {
-        __atomic_store(this->data_, &value, static_cast<integer>(order));
-    }
+constexpr bool original::atomicImpl<TYPE, false>::isLockFree() noexcept {
+    return true;
 }
 
 template <typename TYPE>
-TYPE original::atomic<TYPE>::load(memOrder order) const noexcept
-{
+void original::atomicImpl<TYPE, false>::store(TYPE value, memOrder order) {
+    __atomic_store(reinterpret_cast<TYPE*>(this->data_), &value, static_cast<int>(order));
+}
+
+template <typename TYPE>
+TYPE original::atomicImpl<TYPE, false>::load(memOrder order) const noexcept {
     TYPE result;
-    if constexpr (ORIGINAL_USE_LOCK) {
-        uniqueLock lock{this->mutex_};
-        std::memcpy(&result, this->data_, sizeof(TYPE));
-        return result;
-    } else {
-        __atomic_load(this->data_, &result, static_cast<integer>(order));
-        return result;
-    }
+    __atomic_load(reinterpret_cast<const TYPE*>(this->data_), &result, static_cast<int>(order));
+    return result;
 }
 
 template <typename TYPE>
-TYPE original::atomic<TYPE>::exchange(const TYPE& value, memOrder order) noexcept
-{
+TYPE original::atomicImpl<TYPE, false>::exchange(const TYPE& value, memOrder order) noexcept {
     TYPE result;
-    if constexpr (ORIGINAL_USE_LOCK) {
-        uniqueLock lock{this->mutex_};
-        std::memcpy(&result, this->data_, sizeof(TYPE));
-        std::memcpy(this->data_, &value, sizeof(TYPE));
-        return result;
-    } else {
-        __atomic_exchange(this->data_, &value, &result, static_cast<integer>(order));
-        return result;
-    }
+    __atomic_exchange(reinterpret_cast<TYPE*>(this->data_), const_cast<TYPE*>(&value),
+                      &result, static_cast<int>(order));
+    return result;
 }
 
+template <typename TYPE>
+original::atomicImpl<TYPE, true>::atomicImpl(TYPE value, memOrder) {
+    uniqueLock lock{this->mutex_};
+    this->data_.set(value);
+}
+
+template <typename TYPE>
+constexpr bool original::atomicImpl<TYPE, true>::isLockFree() noexcept {
+    return true;
+}
+
+template <typename TYPE>
+void original::atomicImpl<TYPE, true>::store(TYPE value, memOrder) {
+    uniqueLock lock{this->mutex_};
+    this->data_.set(value);
+}
+
+template <typename TYPE>
+TYPE original::atomicImpl<TYPE, true>::load(memOrder) const noexcept {
+    uniqueLock lock{this->mutex_};
+    return *this->data_;
+}
+
+template <typename TYPE>
+TYPE original::atomicImpl<TYPE, true>::exchange(const TYPE& value, memOrder) noexcept {
+    uniqueLock lock{this->mutex_};
+    TYPE result = *this->data_;
+    this->data_.set(value);
+    return result;
+}
+
+template <typename TYPE>
+auto original::atomic()
+{
+    return atomicImpl<
+        TYPE,
+        !( std::is_trivially_copyable_v<TYPE> &&
+           std::is_trivially_destructible_v<TYPE> &&
+           __atomic_always_lock_free(sizeof(TYPE), nullptr) )
+    >{};
+}
+
+template <typename TYPE>
+auto original::atomic(TYPE value)
+{
+    return atomicImpl<
+        TYPE,
+        !( std::is_trivially_copyable_v<TYPE> &&
+           std::is_trivially_destructible_v<TYPE> &&
+           __atomic_always_lock_free(sizeof(TYPE), nullptr) )
+    >{std::move(value)};
+}
 #endif
-#endif //ORIGINAL_ATOMIC_H
+#endif
