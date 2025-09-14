360030a4e0aa046dc7aa448315768dcf92ef81c3
FrozenlemonTee <1115306170@qq.com>
2025-09-14 21:33:17 +0800

    refactor: Change async::promise to a reusable version of the thread pool

--- src/vibrant/async.h
+++ src/vibrant/async.h
@@ -4,9 +4,8 @@
 #include "atomic.h"
 #include "condition.h"
 #include "optional.h"
-#include "thread.h"
 #include "refCntPtr.h"
-#include "types.h"
+#include "thread.h"
 #include <exception>
 #include <functional>
 #include <utility>
@@ -22,51 +21,31 @@ namespace original {
             std::exception_ptr e_;
 
         public:
-            explicit asyncWrapper() = default;
+            asyncWrapper();
 
-            template<typename Callback, typename... Args>
-            explicit asyncWrapper(Callback&& c, Args&&... args);
+            void setValue(TYPE&& v);
+
+            void setException(std::exception_ptr e);
 
             [[nodiscard]] bool ready() const;
-
-            [[nodiscard]] bool available() const;
-
-            void rethrowIfException() const;
 
             void wait();
 
             TYPE get();
+
+            void rethrowIfException() const;
+
+            [[nodiscard]] bool available() const;
         };
 
     public:
-        class futureBase {
-        public:
-            virtual ~futureBase() = default;
-            [[nodiscard]] virtual bool ready() const = 0;
-            virtual void wait() const = 0;
-            virtual void rethrowIfException() const = 0;
-        };
-
-        template<typename F_TYPE>
-        class future final : public futureBase {
-            mutable strongPtr<asyncWrapper<F_TYPE>> awr_;
+        template<typename TYPE>
+        class future {
+            strongPtr<asyncWrapper<TYPE>> awr_;
 
             friend class async;
-
-            template<typename Callback, typename... Args>
-            explicit future(Callback&& c, Args&&... args);
-
-            template<typename ParentReturn, typename Callback>
-            explicit future(strongPtr<asyncWrapper<ParentReturn>>& parent, Callback&& next);
-
-            bool ready() const override;
-
-            void wait() const override;
-
-            void rethrowIfException() const override;
+            explicit future(strongPtr<asyncWrapper<TYPE>> awr);
         public:
-            using ReturnType = F_TYPE;
-
             future(const future&) = delete;
 
             future& operator=(const future&) = delete;
@@ -75,37 +54,34 @@ namespace original {
 
             future& operator=(future&&) = default;
 
-            F_TYPE result() const;
+            TYPE result();
 
-            template <typename Callback>
-            auto then(Callback&& next);
+            [[nodiscard]] bool ready() const;
+
+            void wait();
         };
 
-        template<typename P_TYPE, typename Callback>
+        template<typename TYPE, typename Callback>
         class promise {
-            std::function<Callback> c;
+            std::function<TYPE()> c_;
+            strongPtr<asyncWrapper<TYPE>> awr_;
 
         public:
-            explicit promise();
+            explicit promise(Callback&& c);
 
-            template<typename F>
-            explicit promise(F f);
+            future<TYPE> getFuture();
 
-            template<typename... Args>
-            future<P_TYPE> getFuture(Args&&... args) const;
-
-            template<typename... Args>
-            P_TYPE operator()(Args&&... args) const;
+            void run();
         };
 
-        template<typename Callback>
-        static auto makePromise(Callback&& c);
+        template <typename Callback, typename... Args>
+        static auto makePromise(Callback&& c, Args&&... args);
 
         template <typename Callback, typename... Args>
-        static auto get(Callback&& c, Args&&... args) -> std::invoke_result_t<std::decay_t<Callback>, std::decay_t<Args>...>;
+        static auto get(Callback&& c, Args&&... args);
     };
 
-    template<>
+    template <>
     class async::asyncWrapper<void> {
         atomic<bool> ready_;
         alternative<void> alter_;
@@ -114,77 +90,77 @@ namespace original {
         std::exception_ptr e_;
 
     public:
-        explicit asyncWrapper();
+        asyncWrapper();
 
-        template<typename Callback, typename... Args>
-        explicit asyncWrapper(Callback&& c, Args&&... args);
+        void setValue();
+
+        void setException(std::exception_ptr e);
 
         [[nodiscard]] bool ready() const;
-
-        [[nodiscard]] bool available() const;
-
-        void rethrowIfException() const;
 
         void wait();
 
         void get();
+
+        void rethrowIfException() const;
+
+        [[nodiscard]] bool available() const;
     };
 
-    template<>
-    class async::future<void> final : public futureBase {
-        mutable strongPtr<asyncWrapper<void>> awr_;
+    template <>
+    class async::future<void> {
+        strongPtr<asyncWrapper<void>> awr_;
 
         friend class async;
+        explicit future(strongPtr<asyncWrapper<void>> awr) : awr_(std::move(awr)) {}
 
-        template<typename Callback, typename... Args>
-        explicit future(Callback&& c, Args&&... args);
-
-        template<typename ParentType, typename Callback>
-        explicit future(strongPtr<asyncWrapper<ParentType>>& parent, Callback&& next);
-
-        bool ready() const override;
-
-        void wait() const override;
-
-        void rethrowIfException() const override;
     public:
         future(const future&) = delete;
-
         future& operator=(const future&) = delete;
-
         future(future&&) = default;
-
         future& operator=(future&&) = default;
 
-        void result() const;
+        void result();
 
-        template <typename Callback>
-        auto then(Callback&& next);
+        [[nodiscard]] bool ready() const;
+
+        void wait();
     };
-}
 
+    template <typename Callback>
+    class async::promise<void, Callback> {
+        std::function<void()> c_;
+        strongPtr<asyncWrapper<void>> awr_;
+
+    public:
+        explicit promise(Callback&& c);
+
+        future<void> getFuture() const;
+
+        void run();
+    };
+} // namespace original
 
 template <typename TYPE>
-template <typename Callback, typename ... Args>
-original::async::asyncWrapper<TYPE>::asyncWrapper(Callback&& c, Args&&... args) : ready_(makeAtomic(false))
+original::async::asyncWrapper<TYPE>::asyncWrapper()
+    : ready_(makeAtomic(false)) {}
+
+template <typename TYPE>
+void original::async::asyncWrapper<TYPE>::setValue(TYPE&& v)
 {
-    thread t{
-        [this, c = std::forward<Callback>(c), ...args = std::forward<Args>(args)]() mutable
-        {
-            uniqueLock lock{this->mutex_};
-            try
-            {
-                this->alter_.set(c(std::forward<Args>(args)...));
-            }
-            catch (...)
-            {
-                this->e_ = std::current_exception();
-            }
-            this->ready_.store(true);
-            this->cond_.notify();
-        },
-        thread::AUTO_DETACH
-    };
+    uniqueLock lock{this->mutex_};
+    this->alter_.set(std::move(v));
+    this->ready_.store(true);
+    this->cond_.notify();
+}
+
+template <typename TYPE>
+void original::async::asyncWrapper<TYPE>::setException(std::exception_ptr e)
+{
+    uniqueLock lock{this->mutex_};
+    this->e_ = std::move(e);
+    this->ready_.store(true);
+    this->cond_.notify();
 }
 
 template <typename TYPE>
@@ -194,176 +170,132 @@ bool original::async::asyncWrapper<TYPE>::ready() const
 }
 
 template <typename TYPE>
+void original::async::asyncWrapper<TYPE>::wait()
+{
+    if (!this->ready())
+        this->cond_.wait(this->mutex_);
+}
+
+template <typename TYPE>
+TYPE original::async::asyncWrapper<TYPE>::get()
+{
+    this->wait();
+    this->rethrowIfException();
+    TYPE result = *this->alter_;
+    this->alter_.reset();
+    return result;
+}
+
+template <typename TYPE>
+void original::async::asyncWrapper<TYPE>::rethrowIfException() const
+{
+    if (this->e_)
+        std::rethrow_exception(this->e_);
+}
+
+template <typename TYPE>
 bool original::async::asyncWrapper<TYPE>::available() const
 {
     return this->ready() && this->alter_.hasValue();
 }
 
 template <typename TYPE>
-void original::async::asyncWrapper<TYPE>::rethrowIfException() const
+original::async::future<TYPE>::future(strongPtr<asyncWrapper<TYPE>> awr)
+    : awr_(awr) {}
+
+template <typename TYPE>
+TYPE original::async::future<TYPE>::result()
 {
-    if (this->e_) {
-        std::rethrow_exception(this->e_);
-    }
+    return this->awr_->get();
 }
 
 template <typename TYPE>
-void original::async::asyncWrapper<TYPE>::wait()
-{
-    this->cond_.wait(this->mutex_);
-}
-
-template <typename TYPE>
-TYPE original::async::asyncWrapper<TYPE>::get()
-{
-    TYPE result = *this->alter_;
-    this->alter_.reset();
-    return result;
-}
-
-template <typename F_TYPE>
-template <typename Callback, typename ... Args>
-original::async::future<F_TYPE>::future(Callback&& c, Args&&... args)
-    : awr_(makeStrongPtr<asyncWrapper<F_TYPE>>(std::forward<Callback>(c), std::forward<Args>(args)...)) {}
-
-template <typename F_TYPE>
-template<typename ParentReturn, typename Callback>
-original::async::future<F_TYPE>::future(strongPtr<asyncWrapper<ParentReturn>>& parent, Callback&& next) {
-    this->awr_ = makeStrongPtr<asyncWrapper<F_TYPE>>(
-        [parent, next = std::forward<Callback>(next)]() mutable -> F_TYPE {
-            if (!parent->ready()) {
-                parent->wait();
-            }
-            parent->rethrowIfException();
-
-            if constexpr (!std::is_void_v<ParentReturn>) {
-                if (parent->available()) {
-                    auto v = parent->get();
-                    return next(std::move(v));
-                }
-                throw valueError("Original value not available");
-            } else {
-                parent->get();
-                if constexpr (std::is_void_v<F_TYPE>) {
-                    next();
-                    return;
-                } else {
-                    return next();
-                }
-            }
-        }
-    );
-}
-
-template <typename F_TYPE>
-bool original::async::future<F_TYPE>::ready() const
+bool original::async::future<TYPE>::ready() const
 {
     return this->awr_->ready();
 }
 
-template <typename F_TYPE>
-void original::async::future<F_TYPE>::wait() const
+template <typename TYPE>
+void original::async::future<TYPE>::wait()
 {
     this->awr_->wait();
 }
 
-template <typename F_TYPE>
-void original::async::future<F_TYPE>::rethrowIfException() const
+template <typename TYPE, typename Callback>
+original::async::promise<TYPE, Callback>::promise(Callback&& c)
+    : c_(std::forward<Callback>(c)), awr_(makeStrongPtr<asyncWrapper<TYPE>>()) {}
+
+template <typename TYPE, typename Callback>
+original::async::future<TYPE>
+original::async::promise<TYPE, Callback>::getFuture()
 {
-    this->awr_->rethrowIfException();
+    return future<TYPE>(this->awr_);
 }
 
-template <typename F_TYPE>
-F_TYPE original::async::future<F_TYPE>::result() const
+template <typename TYPE, typename Callback>
+void original::async::promise<TYPE, Callback>::run()
 {
-    if (!this->awr_->ready()) {
-        this->awr_->wait();
+    try {
+        if constexpr (!std::is_void_v<TYPE>) {
+            this->awr_->setValue(this->c_());
+        } else {
+            this->c_();
+            this->awr_->setValue({});
+        }
+    } catch (...) {
+        this->awr_->setException(std::current_exception());
     }
-
-    this->awr_->rethrowIfException();
-
-    if (this->awr_->available()) {
-        return this->awr_->get();
-    }
-
-    throw valueError("Get an unavailable value");
-}
-
-template <typename F_TYPE>
-template <typename Callback>
-auto original::async::future<F_TYPE>::then(Callback&& next)
-{
-    using RETURN = std::invoke_result_t<Callback, F_TYPE>;
-    return future<RETURN>(this->awr_, std::forward<Callback>(next));
-}
-
-template <typename P_TYPE, typename Callback>
-original::async::promise<P_TYPE, Callback>::promise()
-    : c([] {}) {}
-
-template <typename P_TYPE, typename Callback>
-template <typename F>
-original::async::promise<P_TYPE, Callback>::promise(F f)
-    : c(std::move(f)) {}
-
-template <typename P_TYPE, typename Callback>
-template <typename... Args>
-original::async::future<P_TYPE>
-original::async::promise<P_TYPE, Callback>::getFuture(Args&&... args) const
-{
-    return future<P_TYPE>{c, std::forward<Args>(args)...};
-}
-
-template <typename P_TYPE, typename Callback>
-template <typename ... Args>
-P_TYPE original::async::promise<P_TYPE, Callback>::operator()(Args&&... args) const
-{
-    return this->getFuture(std::forward<Args>(args)...).result();
-}
-
-template <typename Callback>
-auto original::async::makePromise(Callback&& c)
-{
-    using Traits = functionTraits<std::decay_t<Callback>>;
-    using Return = Traits::ReturnType;
-    using Sig = Traits::Signature;
-    return promise<Return, Sig>(std::forward<Callback>(c));
 }
 
 template <typename Callback, typename... Args>
+auto original::async::makePromise(Callback&& c, Args&&... args) {
+    using Return = std::invoke_result_t<Callback, Args...>;
+    auto bound = [c = std::forward<Callback>(c),
+                  ... args = std::forward<Args>(args)]() mutable -> Return {
+        return c(args...);
+    };
+
+    return promise<Return, decltype(bound)>{std::move(bound)};
+}
+
+template <typename Callback, typename ... Args>
 auto original::async::get(Callback&& c, Args&&... args)
-    -> std::invoke_result_t<std::decay_t<Callback>, std::decay_t<Args>...>
 {
     using ResultType = std::invoke_result_t<std::decay_t<Callback>, std::decay_t<Args>...>;
+    auto task = [c = std::forward<Callback>(c), ... args = std::forward<Args>(args)]() mutable -> ResultType {
+        return c(std::forward<Args>(args)...);
+    };
 
-    auto p = promise<ResultType, ResultType(Args...)>{std::forward<Callback>(c)};
-    return p.getFuture(std::forward<Args>(args)...).result();
+    auto p = makePromise(std::move(task));
+    auto fut = p.getFuture();
+
+    thread t{
+        [p = std::move(p)]() mutable {
+            p.run();
+        },
+        thread::AUTO_DETACH
+    };
+
+    return fut.result();
 }
 
 inline original::async::asyncWrapper<void>::asyncWrapper()
     : ready_(makeAtomic(false)) {}
 
-template <typename Callback, typename ... Args>
-original::async::asyncWrapper<void>::asyncWrapper(Callback&& c, Args&&... args) : ready_(makeAtomic(false))
+inline void original::async::asyncWrapper<void>::setValue()
 {
-    thread t{
-        [this, c = std::forward<Callback>(c), ...args = std::forward<Args>(args)]() mutable
-        {
-            uniqueLock lock{this->mutex_};
-            try
-            {
-                c(std::forward<Args>(args)...);
-                this->alter_.set();
-            }
-            catch (...)
-            {
-                this->e_ = std::current_exception();
-            }
-            this->ready_.store(true);
-            this->cond_.notify();
-        },
-        thread::AUTO_DETACH
-    };
+    uniqueLock lock{this->mutex_};
+    this->alter_.set();
+    this->ready_.store(true);
+    this->cond_.notify();
+}
+
+inline void original::async::asyncWrapper<void>::setException(std::exception_ptr e)
+{
+    uniqueLock lock{this->mutex_};
+    this->e_ = std::move(e);
+    this->ready_.store(true);
+    this->cond_.notify();
 }
 
 inline bool original::async::asyncWrapper<void>::ready() const
@@ -371,60 +303,32 @@ inline bool original::async::asyncWrapper<void>::ready() const
     return this->ready_.load();
 }
 
+inline void original::async::asyncWrapper<void>::wait()
+{
+    if (!this->ready())
+        this->cond_.wait(this->mutex_);
+}
+
+inline void original::async::asyncWrapper<void>::get()
+{
+    this->wait();
+    this->rethrowIfException();
+}
+
+inline void original::async::asyncWrapper<void>::rethrowIfException() const
+{
+    if (this->e_)
+        std::rethrow_exception(this->e_);
+}
+
 inline bool original::async::asyncWrapper<void>::available() const
 {
     return this->ready() && this->alter_.hasValue();
 }
 
-inline void original::async::asyncWrapper<void>::rethrowIfException() const
+inline void original::async::future<void>::result()
 {
-    if (this->e_) {
-        std::rethrow_exception(this->e_);
-    }
-}
-
-inline void original::async::asyncWrapper<void>::wait()
-{
-    this->cond_.wait(this->mutex_);
-}
-
-inline void original::async::asyncWrapper<void>::get()
-{
-    this->alter_.reset();
-}
-
-template <typename Callback, typename ... Args>
-original::async::future<void>::future(Callback&& c, Args&&... args)
-    : awr_(makeStrongPtr<asyncWrapper<void>>(std::forward<Callback>(c), std::forward<Args>(args)...)) {}
-
-template<typename ParentType, typename Callback>
-original::async::future<void>::future(strongPtr<asyncWrapper<ParentType>>& parent, Callback&& next)
-{
-    this->awr_ = makeStrongPtr<asyncWrapper<void>>(
-        [parent, next = std::forward<Callback>(next)]() mutable {
-            if constexpr (!std::is_void_v<ParentType>) {
-                if (!parent->ready()) {
-                    parent->wait();
-                }
-                parent->rethrowIfException();
-
-                if (parent->available()) {
-                    auto v = parent->get();
-                    next(std::move(v));
-                } else {
-                    throw valueError("Parent future returned no value");
-                }
-            } else {
-                if (!parent->ready()) {
-                    parent->wait();
-                }
-                parent->rethrowIfException();
-                parent->get();
-
-                next();
-            }
-        }
-    );
+    this->awr_->get();
 }
 
 inline bool original::async::future<void>::ready() const
@@ -432,37 +336,30 @@ inline bool original::async::future<void>::ready() const
     return this->awr_->ready();
 }
 
-inline void original::async::future<void>::wait() const
+inline void original::async::future<void>::wait()
 {
     this->awr_->wait();
 }
 
-inline void original::async::future<void>::rethrowIfException() const
+template <typename Callback>
+original::async::promise<void, Callback>::promise(Callback&& c)
+    : c_(std::forward<Callback>(c)), awr_(makeStrongPtr<asyncWrapper<void>>()) {}
+
+template <typename Callback>
+original::async::future<void> original::async::promise<void, Callback>::getFuture() const
 {
-    this->awr_->rethrowIfException();
-}
-
-inline void original::async::future<void>::result() const
-{
-    if (!this->awr_->ready()) {
-        this->awr_->wait();
-    }
-
-    this->awr_->rethrowIfException();
-
-    if (this->awr_->available()) {
-        this->awr_->get();
-        return;
-    }
-
-    throw valueError("Not available");
+    return future<void>(this->awr_);
 }
 
 template <typename Callback>
-auto original::async::future<void>::then(Callback&& next)
+void original::async::promise<void, Callback>::run()
 {
-    using RETURN = std::invoke_result_t<Callback>;
-    return future<RETURN>(this->awr_, std::forward<Callback>(next));
+    try {
+        this->c_();
+        this->awr_->setValue();
+    } catch (...) {
+        this->awr_->setException(std::current_exception());
+    }
 }
 
 #endif // ORIGINAL_ASYNC_H
