dc383db456811e0be601b04c8259707336b72804
FrozenlemonTee <frozenlemontee@gmail.com>
2025-09-15 18:54:54 +0800

    feat: Add missing functions and fix bugs

--- src/vibrant/tasks.h
+++ src/vibrant/tasks.h
@@ -2,9 +2,11 @@
 #define ORIGINAL_TASKS_H
 
 #include "async.h"
+#include "atomic.h"
 #include "queue.h"
 #include "refCntPtr.h"
 #include "array.h"
+#include "vector.h"
 
 
 namespace original {
@@ -43,14 +45,23 @@ namespace original {
     public:
         explicit taskDelegator(u_integer thread_cnt);
 
+        template<typename Callback, typename... Args>
+        auto submit(Callback&& c, Args&&... args);
+
         u_integer tasksCount() const noexcept;
+
+        void stop();
+
+        ~taskDelegator();
     };
 }
 
 template <typename TYPE>
-template <typename Callback, typename ... Args>
+template <typename Callback, typename... Args>
 original::task<TYPE>::task(Callback&& c, Args&&... args)
-    : p(async::makePromise(std::forward<Callback>(c), std::forward<Args>(args)...)) {}
+    : p([c = std::forward<Callback>(c), ...args = std::forward<Args>(args)]() mutable {
+        return c(args...);
+    }) {}
 
 template <typename TYPE>
 void original::task<TYPE>::run()
@@ -67,7 +78,7 @@ original::async::future<TYPE> original::task<TYPE>::getFuture()
 inline original::taskDelegator::taskDelegator(const u_integer thread_cnt = 8)
     : threads_(thread_cnt), stopped_(makeAtomic(false)) {
     for (auto& thread_ : this->threads_) {
-        thread new_thread {
+        thread_ = thread {
             [this]{
                 while (true) {
                     strongPtr<taskBase> task;
@@ -76,7 +87,7 @@ inline original::taskDelegator::taskDelegator(const u_integer thread_cnt = 8)
                         this->condition_.wait(this->mutex_, [this] {
                             return *this->stopped_ || !this->tasks_.empty();
                         });
-                        if (this->stopped_ && this->started_ == 0 && this->tasks_.empty()) {
+                        if (this->stopped_ && this->tasks_.empty()) {
                             return;
                         }
                         task = std::move(this->tasks_.pop());
@@ -85,14 +96,55 @@ inline original::taskDelegator::taskDelegator(const u_integer thread_cnt = 8)
                 }
             }
         };
-        thread_ = std::move(new_thread);
     }
+}
+
+template <typename Callback, typename ... Args>
+auto original::taskDelegator::submit(Callback&& c, Args&&... args)
+{
+    using ReturnType = decltype(c(args...));
+    auto new_task = makeStrongPtr<task<ReturnType>>(
+        std::forward<Callback>(c),
+        std::forward<Args>(args)...
+    );
+
+    auto f = new_task->getFuture();
+
+    {
+        uniqueLock lock(this->mutex_);
+        if (this->stopped_) {
+            throw sysError("taskDelegator already stopped");
+        }
+        this->tasks_.push(std::move(new_task)); // todo: impl strongPtr::staticCastTo<T>
+    }
+
+    this->condition_.notify();
+    return f;
 }
 
 inline original::u_integer
 original::taskDelegator::tasksCount() const noexcept
 {
     return this->tasks_.size();
+}
+
+inline void original::taskDelegator::stop()
+{
+    {
+        uniqueLock lock(this->mutex_);
+        this->stopped_.store(true);
+    }
+    this->condition_.notifyAll();
+}
+
+inline original::taskDelegator::~taskDelegator()
+{
+    this->stop();
+    for (auto& thread_ : this->threads_) {
+        if (thread_.joinable()) {
+            thread_.join();
+        }
+    }
 }
 
 #endif //ORIGINAL_TASKS_H
