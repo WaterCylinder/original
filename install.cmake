c21a465470546f816ae90ad0ac497928c6ada970
FrozenLemonTee <1115306170@qq.com>
2025-09-23 00:40:10 +0800

    chore: Install files integrated to a single directory

--- install.cmake
+++ install.cmake
@@ -7,17 +7,18 @@ cmake_minimum_required(VERSION 3.31)
 # cmake -P install.cmake -DCLEAN_BUILD_DIR=OFF
 option(CLEAN_BUILD_DIR "Remove build directory after install" ON)
 
-get_filename_component(PROJECT_ROOT "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
+get_filename_component(ORIGINAL_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
 
-set(INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR})
-message(STATUS "Install directory: ${INSTALL_DIR}")
+set(CURRENT_DIR ${CMAKE_CURRENT_BINARY_DIR})
+message(STATUS "Install directory: ${CURRENT_DIR}")
 
-file(MAKE_DIRECTORY ${INSTALL_DIR}/build)
+set(ORIGINAL_BUILD_DIR ${CURRENT_DIR}/build)
+file(MAKE_DIRECTORY ${ORIGINAL_BUILD_DIR})
 
 message(STATUS "Cmake config initializing...")
 execute_process(
-        COMMAND ${CMAKE_COMMAND} ${PROJECT_ROOT} -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF
-        WORKING_DIRECTORY ${INSTALL_DIR}/build
+        COMMAND ${CMAKE_COMMAND} ${ORIGINAL_ROOT_DIR} -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF
+        WORKING_DIRECTORY ${ORIGINAL_BUILD_DIR}
         RESULT_VARIABLE result
 )
 
@@ -28,7 +29,7 @@ endif()
 message(STATUS "Building project...")
 execute_process(
         COMMAND ${CMAKE_COMMAND} --build . --config Release
-        WORKING_DIRECTORY ${INSTALL_DIR}/build
+        WORKING_DIRECTORY ${ORIGINAL_BUILD_DIR}
         RESULT_VARIABLE result
 )
 
@@ -36,10 +37,12 @@ if(NOT result EQUAL 0)
     message(FATAL_ERROR "CMake build failed with result ${result}")
 endif()
 
+set(ORIGINAL_INSTALL_DIR ${CURRENT_DIR}/original)
+
 message(STATUS "Installing library files and Header files of project...")
 execute_process(
-        COMMAND ${CMAKE_COMMAND} --install . --config Release --prefix ${INSTALL_DIR}
-        WORKING_DIRECTORY ${INSTALL_DIR}/build
+        COMMAND ${CMAKE_COMMAND} --install . --config Release --prefix ${ORIGINAL_INSTALL_DIR}
+        WORKING_DIRECTORY ${ORIGINAL_BUILD_DIR}
         RESULT_VARIABLE result
 )
 
@@ -50,8 +53,8 @@ endif()
 message(STATUS "Install success!")
 if(CLEAN_BUILD_DIR)
     message(STATUS "Removing temporary build files...")
-    file(REMOVE_RECURSE ${INSTALL_DIR}/build)
+    file(REMOVE_RECURSE ${ORIGINAL_BUILD_DIR})
     message(STATUS "Temporary build files removed...")
 endif()
-message(STATUS "Library files at: ${INSTALL_DIR}/lib/")
-message(STATUS "Header files at: ${INSTALL_DIR}/include/")
+message(STATUS "Library files at: ${ORIGINAL_INSTALL_DIR}/lib/")
+message(STATUS "Header files at: ${ORIGINAL_INSTALL_DIR}/include/")
