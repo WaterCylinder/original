73d6f6e8b7db51c869e04924a0b73a2d7099d99c
FrozenLemonTee <1115306170@qq.com>
2025-09-25 00:32:48 +0800

    test: Add tests for semaphore

---
+++ test/unit_test/test_vibrant/test_semaphores.cpp
@@ -0,0 +1,90 @@
+#include <gtest/gtest.h>
+#include "thread.h"
+#include <vector>
+#include "zeit.h"
+#include "semaphores.h"
+
+using namespace original;
+using namespace original::literals;
+
+TEST(SemaphoreTest, AcquireReleaseBasic) {
+    semaphore<3> sem(2);
+
+    EXPECT_TRUE(sem.tryAcquire());  // count=1
+    EXPECT_TRUE(sem.tryAcquire());  // count=0
+    EXPECT_FALSE(sem.tryAcquire()); // 没有资源了
+
+    sem.release();
+    EXPECT_TRUE(sem.tryAcquire());  // 又能获取了
+}
+
+TEST(SemaphoreTest, BlockingAcquire) {
+    semaphore<1> sem(0);
+
+    thread t([&] {
+        thread::sleep(100_ms);
+        sem.release();
+    });
+
+    auto start = time::point::now();
+    sem.acquire(); // 应该会阻塞直到 release
+    auto end = time::point::now();
+
+    EXPECT_GE(end - start, 100_ms);
+    t.join();
+}
+
+TEST(SemaphoreTest, AcquireTimeout) {
+    semaphore<1> sem(0);
+
+    auto start = time::point::now();
+    bool ok = sem.acquireFor(200_ms);
+    auto end = time::point::now();
+
+    EXPECT_FALSE(ok); // 超时失败
+    EXPECT_GE(end - start, 200_ms);
+}
+
+TEST(SemaphoreTest, ReleaseBlocksWhenFull) {
+    semaphore<2> sem(2);
+
+    thread t([&] {
+        thread::sleep(200_ms);
+        sem.acquire(); // 消费一个
+    });
+
+    auto start = time::point::now();
+    sem.release(); // 应该会阻塞，直到 acquire 消费一个
+    auto end = time::point::now();
+
+    EXPECT_GE(end - start, 200_ms);
+    t.join();
+}
+
+TEST(SemaphoreTest, ReleaseTimeout) {
+    semaphore<2> sem(2);
+
+    bool ok = sem.releaseFor(1, 200_ms);
+    EXPECT_FALSE(ok); // 已经满了，超时失败
+}
+
+TEST(SemaphoreTest, UnboundedSemaphore) {
+    semaphore<0> sem(0);
+
+    EXPECT_FALSE(sem.tryAcquire());
+    sem.release(5);
+
+    for (int i = 0; i < 5; i++) {
+        EXPECT_TRUE(sem.tryAcquire());
+    }
+    EXPECT_FALSE(sem.tryAcquire());
+}
+
+TEST(SemaphoreGuardTest, RAII) {
+    semaphore<2> sem(1);
+    {
+        semaphoreGuard<2> guard(sem);
+        EXPECT_FALSE(sem.tryAcquire()); // 已经被 guard 拿走了
+    }
+    EXPECT_TRUE(sem.tryAcquire()); // guard 析构时释放
+}
