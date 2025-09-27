ec420b05c3d02f1f35bd177ab745ad9f4e055b30
frozenlemontee <frozenlemontee@gmail.com>
2025-09-27 13:27:26 +0800

    test: Add more tests

--- test/unit_test/test_vibrant/test_syncPoint.cpp
+++ test/unit_test/test_vibrant/test_syncPoint.cpp
@@ -126,3 +126,140 @@ TEST(SyncPointTest, NoFunctionProvided) {
     EXPECT_EQ(arrived, THREAD_COUNT);
     // 无完成函数也应正常同步
 }
+
+TEST(SyncPointTest, ManyThreads) {
+    constexpr int THREAD_COUNT = 10;
+    constexpr int ROUNDS = 10;
+    std::atomic arrived_count{0};
+    std::atomic complete_called{false};
+
+    syncPoint sp(THREAD_COUNT, [&complete_called] {
+        complete_called = true;
+    });
+
+    std::vector<thread> threads;
+    for (int i = 0; i < THREAD_COUNT * ROUNDS; ++i) {
+        threads.emplace_back([&arrived_count, &sp] {
+            ++arrived_count;
+            sp.arrive();
+        });
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    EXPECT_EQ(arrived_count, THREAD_COUNT * ROUNDS);
+    EXPECT_TRUE(complete_called);
+}
+
+TEST(SyncPointTest, SequentialArrival) {
+    constexpr int THREAD_COUNT = 3;
+    syncPoint sp(THREAD_COUNT);
+    std::atomic completion_count{0};
+
+    // 重新设置完成函数
+    syncPoint sp_with_func(THREAD_COUNT, [&completion_count] {
+        ++completion_count;
+    });
+
+    std::vector<thread> threads;
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        threads.emplace_back([&sp_with_func, i] {
+            thread::sleep(i * 10_ms); // 顺序启动
+            sp_with_func.arrive();
+        });
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    EXPECT_EQ(completion_count, 1);
+}
+
+TEST(SyncPointTest, DataConsistency) {
+    constexpr int THREAD_COUNT = 4;
+    constexpr int ITERATIONS = 100;
+    std::atomic counter{0};
+    std::atomic sync_count{0};
+
+    syncPoint sp(THREAD_COUNT, [&sync_count] {
+        ++sync_count;
+    });
+
+    auto worker = [&]() {
+        for (int i = 0; i < ITERATIONS; ++i) {
+            // 做一些工作
+            ++counter;
+            sp.arrive();
+        }
+    };
+
+    std::vector<thread> threads;
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        threads.emplace_back(worker);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    EXPECT_EQ(counter, THREAD_COUNT * ITERATIONS);
+    EXPECT_EQ(sync_count, ITERATIONS);
+}
+
+TEST(SyncPointTest, ZeroMaxArrived) {
+    syncPoint sp(0);
+
+    auto start = time::point::now();
+    sp.arrive();
+    auto end = time::point::now();
+
+    EXPECT_LT(end - start, 50_ms);
+
+    std::atomic call_count{0};
+    std::vector<thread> threads;
+
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back([&sp, &call_count] {
+            sp.arrive();
+            ++call_count;
+        });
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    EXPECT_EQ(call_count, 5);
+}
+
+TEST(SyncPointTest, ExceptionInCompleteFunction) {
+    constexpr int THREAD_COUNT = 2;
+    std::atomic exception_caught{false};
+    std::atomic threads_continued{false};
+
+    syncPoint sp(THREAD_COUNT, [] {
+        throw std::runtime_error("Test exception");
+    });
+
+    auto worker = [&] {
+        try {
+            sp.arrive();
+            threads_continued = true;
+        } catch (...) {
+            exception_caught = true;
+        }
+    };
+
+    thread t1(worker);
+    thread t2(worker);
+
+    t1.join();
+    t2.join();
+
+    // 异常应该被捕获，线程应该继续执行
+    EXPECT_TRUE(exception_caught);
+    EXPECT_TRUE(threads_continued);
+}
